fe6934ae0e85a7ac6c138637044d915444cc6505
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-09-25 16:41:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+import sounddevice as sd
+import numpy as np
+from transformers import pipeline
+import queue
+import time
+from app.translation import load_translator, translate
+from app.tts import synthesize_speech
+from app import config
