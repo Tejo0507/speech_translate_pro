d7409fb12a7c5a170d70bdc6e611ced0ceea370c
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-09-30 22:30:18 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-MODEL_STT = "openai/whisper-medium"
-MODEL_TRANSLATION = "Helsinki-NLP/opus-mt-en-mul"  # Supports English to multiple languages including Telugu, Tamil, Hindi
-DEVICE = 0
-LANGUAGE = "en"
-SAMPLERATE = 16000
-CHUNK_SECONDS = 5
