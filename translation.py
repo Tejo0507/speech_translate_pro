225ffb51ad1c93c9fd3cef8008af4d1ff35ffa96
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-09-30 22:31:04 +0530

    Delete translation.py

--- translation.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-from app import config
-
-def load_translator():
-    return pipeline("translation", model=config.MODEL_TRANSLATION, device=config.DEVICE)
-
-def translate(translator, text, tgt_lang):
-    return translator(text, tgt_lang=tgt_lang)[0]["translation_text"]
